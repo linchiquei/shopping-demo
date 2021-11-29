481e33eb7d5a2386354c594ecd86c43c7e38e7cf
ricky lin <ricky@bitoex.com>
2021-11-29 17:38:04 +0800

    users can see their orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,12 @@
 class OrdersController < ApplicationController
+
+
   def create
     @order = Order.new(order_params)
     @order.user = current_user
     @order.total = current_cart.total_price
     if @order.save
-      current_cart.cart_items.each do |item|
+      current_cart.cart_items.each do |cart_item|
         product_list = ProductList.new
         product_list.order = @order
         product_list.product_name = cart_item.product.title
@@ -12,10 +14,15 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
-      redirect_to order_path(@order)
+      redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
     end
+  end
+
+  def show
+    @order = Order.find_by_token(params[:id])
+    @product_lists = @order.product_lists
   end
 
   private
---
+++ db/migrate/20211129084256_add_token_to_order.rb
@@ -0,0 +1,6 @@
+class AddTokenToOrder < ActiveRecord::Migration[6.1]
+  def change
+    add_column :orders, :token, :string
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
